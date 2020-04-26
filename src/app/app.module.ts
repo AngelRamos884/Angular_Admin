1dd97391d512ed3addc14b7fb756bb2b1212fe0b
JUAN VAZQUEZ <JUAN_VAZQUEZ3@jabil.com>
2020-04-26 16:56:16 -0700

    modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Routes
+import { APP_ROUTES } from './app.routes';
+
+//Modules
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    LoginComponent     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PagesModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
